b716de5cbd6e118ce3d321356dad85c70b0c34c5
madneal <neal1991@sina.com>
2022-03-18 17:17:19 +0800

    format the code

--- scripts/background.js
+++ scripts/background.js
@@ -1,60 +1,60 @@
 "use strict";
 
-chrome.browserAction.setBadgeText({ text: "" });
+chrome.browserAction.setBadgeText({text: ""});
 sourceMap.SourceMapConsumer.initialize({
-        "lib/mappings.wasm": "https://unpkg.com/source-map@0.7.3/lib/mappings.wasm"
-    });
+    "lib/mappings.wasm": "https://unpkg.com/source-map@0.7.3/lib/mappings.wasm"
+});
 
 let sourceFileList = {};
 chrome.tabs.onActivated.addListener(() => {
-  sourceFileList = {};
-  setBadgeText(Object.keys(sourceFileList).length);
+    sourceFileList = {};
+    setBadgeText(Object.keys(sourceFileList).length);
 })
 
 chrome.webRequest.onBeforeRequest.addListener(async (details) => {
-  await chrome.tabs.query({ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT }, async (tabs) => {
-    if (details.type === "script" && /\.js$/.test(details.url)
-    && !/^chrome-extension:\/\//.test(details.url)) {
+    await chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, async (tabs) => {
+        if (details.type === "script" && /\.js$/.test(details.url)
+            && !/^chrome-extension:\/\//.test(details.url)) {
 
-    const text = await request(details.url + ".map");
-        if (text) {
-    const consumer = await new sourceMap.SourceMapConsumer(text);
-    sourceFileList[details.url] = {
-      content: consumer.sourcesContent,
-      sources: consumer._absoluteSources,
-      page: tabs[0]
-    }
-    consumer.destroy();
-  }
-}
-});
-setBadgeText(Object.keys(sourceFileList).length);
+            const text = await request(details.url + ".map");
+            if (text) {
+                const consumer = await new sourceMap.SourceMapConsumer(text);
+                sourceFileList[details.url] = {
+                    content: consumer.sourcesContent,
+                    sources: consumer._absoluteSources,
+                    page: tabs[0]
+                }
+                consumer.destroy();
+            }
+        }
+    });
+    setBadgeText(Object.keys(sourceFileList).length);
 
 }, {
-  urls: ["<all_urls>"]
+    urls: ["<all_urls>"]
 });
 
 const request = async (url) => {
-  const res = await fetch(url)
-  if (res.ok) {
-    return res.text();
-  } else {
-    console.info("Request to " + url + " error with status " + res.status);
-  }
+    const res = await fetch(url)
+    if (res.ok) {
+        return res.text();
+    } else {
+        console.info("Request to " + url + " error with status " + res.status);
+    }
 }
 
 const setBadgeText = function setBadgeText(num) {
-  const text = num > 0 ? "" + num : "";
-  chrome.browserAction.setBadgeText({ text: text });
+    const text = num > 0 ? "" + num : "";
+    chrome.browserAction.setBadgeText({text: text});
 };
 
 chrome.extension.onConnect.addListener(function (port) {
-  port.postMessage(Object.keys(sourceFileList).map(function (key) {
-    return {
-      url: key,
-      content: sourceFileList[key].content,
-      page: sourceFileList[key].page,
-      filenames: sourceFileList[key].sources
-    };
-  }));
+    port.postMessage(Object.keys(sourceFileList).map(function (key) {
+        return {
+            url: key,
+            content: sourceFileList[key].content,
+            page: sourceFileList[key].page,
+            filenames: sourceFileList[key].sources
+        };
+    }));
 });
--- scripts/options.js
+++ scripts/options.js
@@ -1,11 +1,11 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-const btn = document.querySelector('#togBtn');
-if (btn) {
-btn.addEventListener('click', () => {
-    const ifEnabled = document.querySelector('#togBtn').checked;
-        chrome.management.setEnabled(
- "mddmekafepnknkghbbogmhaiknlabdgc",ifEnabled );
-})
-}
+    const btn = document.querySelector('#togBtn');
+    if (btn) {
+        btn.addEventListener('click', () => {
+            const ifEnabled = document.querySelector('#togBtn').checked;
+            chrome.management.setEnabled(
+                "mddmekafepnknkghbbogmhaiknlabdgc", ifEnabled);
+        })
+    }
 })
